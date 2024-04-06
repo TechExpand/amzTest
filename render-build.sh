2d76271f0b0515a2aa0e3946b7ff20dc5f1f5b49
TechExpand <dailydevo9@gmail.com>
2024-04-06 17:40:38 +0100

    d'

--- render-build.sh
+++ render-build.sh
@@ -8,7 +8,7 @@ npm install
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
   echo "...Copying Puppeteer Cache from Build Cache" 
   cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
-else 
-  echo "...Storing Puppeteer Cache in Build Cache" 
-  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+# else 
+#   echo "...Storing Puppeteer Cache in Build Cache" 
+#   cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
 fi
